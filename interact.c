feb3112b7d3a5aee48da54b99a4fadd3b41fc6ba
abschill <ajay198810@gmail.com>
2021-11-24 14:52:18 -0500

    arrow keys

--- interact.c
+++ interact.c
@@ -10,10 +10,12 @@
 #define CTRL_KEY( k ) ( ( k ) & 0x1f )
 #define ABS_VERSION "0.0.1"
 enum editorKey {
-	ARROW_LEFT = 'a',
-	ARROW_RIGHT = 'd',
-	ARROW_UP = 'w',
-	ARROW_DOWN = 's'
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 struct termios orig_termios;
@@ -57,7 +59,7 @@ void enableRawMode() {
 	if( tcsetattr( STDIN_FILENO, TCSAFLUSH, &raw ) == -1 ) die( "tcsetattr" );  
 }
 
-char editorReadKey() {
+int editorReadKey() {
 	int nread;
 	char c;
 	while( (nread = read( STDIN_FILENO, &c, 1 ) ) != 1 ) {
@@ -71,11 +73,30 @@ char editorReadKey() {
 		if( read( STDIN_FILENO, &seq[1], 1 ) != 1 ) return '\x1b';
 
 		if( seq[0] == '[' ) {
-			switch( seq[1] ) {
-				case 'A': return ARROW_UP;
-				case 'B': return ARROW_DOWN;
-				case 'C': return ARROW_RIGHT;
-				case 'D': return ARROW_LEFT;
+			if( seq[1] >= '0' && seq[1] <= '9' ) {
+				if( read( STDIN_FILENO, &seq[2], 1 ) != 1 ) return '\x1b';
+				if( seq[2] == '~' ) {
+					switch( seq[1] ) {
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+					}
+
+				}
+				else {
+					switch( seq[1] ) {					                                        
+						case 'A': return ARROW_UP;
+						case 'B': return ARROW_DOWN;
+						case 'C': return ARROW_RIGHT;
+						case 'D': return ARROW_LEFT;																									      }
+				}
+			}
+			else {
+				switch( seq[1] ) {
+                                        case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+				}
 			}
 		}
 		return '\x1b';
@@ -190,7 +211,7 @@ void editorRefreshScreen() {
 	abFree( &ab );
 }
 
-void editorMoveCursor( char key ) {
+void editorMoveCursor( int key ) {
 	switch( key ) {
 		case ARROW_LEFT:
 			E.cx--;
@@ -208,12 +229,22 @@ void editorMoveCursor( char key ) {
 }
 
 void editorProcessKeypress() {
-	char c = editorReadKey();
+	int c = editorReadKey();
 	switch( c ) {
 		case CTRL_KEY( 'q' ):
 			write( STDOUT_FILENO, "\x1b[2J", 4 );
 			write( STDOUT_FILENO, "\x1b[H", 3 );
 			exit( 0 );
+			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+				int times = E.screenrows;
+				while( times-- ) {
+					editorMoveCursor( c == PAGE_UP ? ARROW_UP : ARROW_DOWN );
+				}
+			}
 			break;
 
 		case ARROW_UP:
